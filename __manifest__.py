011c1123bc900ce7eac0dcf6ca409ff01be75ee4
Facundo Cuellar <41437771+facundocg06@users.noreply.github.com>
2024-11-06 00:30:03 -0400

    inicio

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Agenda Estudiantil',
+    'version': '1.0',
+    'summary': 'Gestión de agenda estudiantil para enviar comunicados',
+    'category': 'Education',
+    'author': 'grupo 33',
+    'depends': ['base'],
+    'data': [
+        'views/agenda_view.xml',
+        'views/agenda_menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
